9946c8731ce6162d0186667f41c5bcb2bb2ef64e
pounceinwindow <adk03042006@gmail.com>
2026-04-29 12:44:53 +0300

    feat: catalog page with property cards

--- src/UpperCube.Web/Controllers/CatalogController.cs
+++ src/UpperCube.Web/Controllers/CatalogController.cs
@@ -1,8 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
+using UpperCube.Application.Abstractions.Repositories;
+using UpperCube.Application.DTOs;
+using UpperCube.Domain.Enums;
+using UpperCube.Web.Mapping;
 
 namespace UpperCube.Web.Controllers;
 
-public class CatalogController : Controller
+public class CatalogController(IPropertyRepository repository) : Controller
 {
-    public IActionResult Index() => View();
+    public async Task<IActionResult> Index()
+    {
+        var model = (await repository
+            .SearchAsync(new PropertySearchFilter(Status:(int)PropertyStatus.Published), 1, 9))
+            .Items
+            .Select(p => p.ToListItemDto());
+        
+        return View(model);
+    }
 }
