cddfa119c803997db2bb79b5e37d259379fb5787
pounceinwindow <adk03042006@gmail.com>
2026-05-15 23:33:55 +0300

    w

--- src/UpperCube.Web/Controllers/FavoriteApiController.cs
+++ src/UpperCube.Web/Controllers/FavoriteApiController.cs
@@ -1,3 +1,4 @@
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UpperCube.Application.Abstractions.Repositories;
@@ -6,6 +7,7 @@ using UpperCube.Infrastructure.Identity;
 
 namespace UpperCube.Web.Controllers;
 
+[Authorize]
 [ApiController]
 [Route("api/favorites")]
 public sealed class FavoritesApiController(
